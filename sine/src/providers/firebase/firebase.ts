6e38f19b9a1dd65f29e2bf30bdf393e177006387
cleybson cardoso <cleybsonc@gmail.com>
2017-08-10 16:39:59 -0300

    firebase

---
+++ sine/src/providers/firebase/firebase.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+/*
+  Generated class for the FirebaseProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular DI.
+*/
+@Injectable()
+export class FirebaseProvider {
+
+  constructor(public afdb: AngularFireDatabase) {
+    console.log('Hello FirebaseProvider Provider');
+  }
+
+    getVagas() {
+    return this.afdb.list('/vagas/');
+  }
+
+  getEqualsOS(valor: string) {
+     return this.afdb.list('/vagas/', {
+      query: {
+        orderByChild: 'status',
+        equalTo: valor
+      }
+    });
+  }
+
+  checkIn(key, valor){
+    return this.afdb.list('/vagas/').update(key, { valor: valor })
+  }
+
+}
