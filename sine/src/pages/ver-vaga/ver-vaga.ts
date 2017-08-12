8a2b2cc7e27f21dd8136a08ce0700e358074af22
Icaro <rockicarorios@gmail.com>
2017-08-12 05:13:25 -0300

    exibindo vaga

--- sine/src/pages/ver-vaga/ver-vaga.ts
+++ sine/src/pages/ver-vaga/ver-vaga.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { NativeStorageProvider } from './../../providers/native-storage/native-storage';
+import { FirebaseProvider } from './../../providers/firebase/firebase';
+import { Vaga } from './../../model/vaga';
 /**
  * Generated class for the VerVagaPage page.
  *
@@ -14,10 +16,29 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class VerVagaPage {
 
-  private vaga;
+  private vaga: Vaga = new Vaga();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.vaga = this.navParams.get("vaga");
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private native: NativeStorageProvider,
+    private fbService: FirebaseProvider
+  ) {
+    this.vaga = this.navParams.get('vaga');
+    console.log(this.vaga);
+
+    if (!this.vaga.cursoTecnico) {
+      this.vaga.cursoTecnico = [{ nome: '' }];
+    } if (!this.vaga.cursoSuperior) {
+      this.vaga.cursoSuperior = [{ nome: '' }];
+    } if (!this.vaga.posGraduacao) {
+      this.vaga.posGraduacao = [{ nome: '' }];
+    } if (!this.vaga.habilidades) {
+      this.vaga.habilidades = [{ nome: '' }];
+    } if(!this.vaga.certificacoes){
+      this.vaga.certificacoes = [{nome: ''}];
+    }
+
   }
 
   ionViewDidLoad() {
