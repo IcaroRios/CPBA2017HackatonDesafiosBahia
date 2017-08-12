cd9b8e006a3940d9c2667eff69e2bd1538f03b2b
cleybson cardoso <cleybsonc@gmail.com>
2017-08-11 22:14:50 -0300

    alterar dados

--- sine/src/model/candidato.ts
+++ sine/src/model/candidato.ts
@@ -2,8 +2,8 @@ export class Candidato {
     public nome: string = "";
     public email: string = "";
     public senha: string = "";
-    private $key;
-    private key;
+    public $key;
+    public key;
     public numeroIdentificacao: string = "";
     public nomeMae: string = "";
     public apelido: string = "";
--- sine/src/pages/dados-usuario/dados-usuario.ts
+++ sine/src/pages/dados-usuario/dados-usuario.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { FirebaseProvider } from '../../providers/firebase/firebase';
 import { Candidato } from '../../model/candidato';
 import { Http } from '@angular/http';
+import { NativeStorageProvider } from '../../providers/native-storage/native-storage';
 
 /**
  * Generated class for the DadosUsuarioPage page.
@@ -25,8 +26,14 @@ export class DadosUsuarioPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     private toastCtrl: ToastController,
-    private http: Http
+    private http: Http,
+    private native: NativeStorageProvider,
+    private fbService: FirebaseProvider
   ) {
+    this.native.get('user').then(usuario => {
+      this.candidato = usuario;
+      console.log(this.candidato);
+    });
   }
 
   mensagem(message) {
@@ -41,38 +48,51 @@ export class DadosUsuarioPage {
     console.log('ionViewDidLoad DadosUsuarioPage');
   }
   salvarInformacoes() {
-    if(!this.validarCPF() ){
+    if (!this.validarCPF()) {
       this.mensagem("CPF inválido");
+    } else {
+      console.log(this.candidato);
+      this.fbService.updateCandidato(this.candidato.key, this.candidato).then(() => {
+        this.mensagem("Alterado com sucesso");
+        this.native.set("user", this.candidato);
+      });
     }
   }
 
   validarCPF() {
-    let numero = this.candidato.cpf;
-    let ultimoDigito = 0;
-    let penultimoDigito = 0;
-    for (let i = 0; i < 10; i++) {
-      if (i < 9) {
-        penultimoDigito += (+numero[i] * (10 - i));
-        ultimoDigito += (+numero[i] * (11 - i));
-      } else
-        ultimoDigito += (+numero[i] * (11 - i));
+    console.log(this.candidato.cpf);
+    if (this.candidato.cpf) {
+      if (this.candidato.cpf.length == 11) {
+        let numero = this.candidato.cpf.toString();
+        let ultimoDigito = 0;
+        let penultimoDigito = 0;
+        for (let i = 0; i < 10; i++) {
+          if (i < 9) {
+            penultimoDigito += (+numero[i] * (10 - i));
+            ultimoDigito += (+numero[i] * (11 - i));
+          } else
+            ultimoDigito += (+numero[i] * (11 - i));
+        }
+        penultimoDigito = penultimoDigito * 10;
+        ultimoDigito = ultimoDigito * 10;
+
+        penultimoDigito = penultimoDigito % 11;
+        ultimoDigito = ultimoDigito % 11;
+        if (penultimoDigito.toString() == numero[numero.length - 2] && ultimoDigito.toString() == numero[numero.length - 1])
+          return true;
+        else
+          return false;
+
+      } else {
+        this.mensagem("CPF invalido");
+      }
     }
-    penultimoDigito = penultimoDigito * 10;
-    ultimoDigito = ultimoDigito * 10;
-
-    penultimoDigito = penultimoDigito % 11;
-    ultimoDigito = ultimoDigito % 11;
-    if (penultimoDigito.toString() == numero[numero.length - 2] && ultimoDigito.toString() == numero[numero.length - 1])
-      return true;
-    else
-      return false;
-
   }
 
   getProfissioes() {
-    this.http.get("http://apps.diogomachado.com/api-profissoes/v1?callback=CALLBACK_JSONP&s="+this.profissao)
-    .toPromise().then(profissoes=>{
-      console.log(profissoes)
-    });
+    this.http.get("http://apps.diogomachado.com/api-profissoes/v1?callback=CALLBACK_JSONP&s=" + this.profissao)
+      .toPromise().then(profissoes => {
+        console.log(profissoes)
+      });
   }
 }
